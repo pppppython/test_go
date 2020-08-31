60082b57e7829e1e2251b8593f0e380b9bbe9044
pppppython <2223601321@qq.com>
2020-08-31 20:51:20 +0800

    向指定DB位置写入浮点数

--- python-plc.py
+++ python-plc.py
@@ -13,21 +13,32 @@ plc.connect("192.168.0.1",rack=0,slot=1)
 
 #读取浮点数
 s=plc.read_area(0x84,1,0,8)    #从DB1的第0个字节开始，连续读取8个字节，一个浮点数占4个字节
-print(get_real(s,0))           #打印第一个浮点数
-print(get_real(s,4))           #打印第二个浮点数
+#print(get_real(s,0))           #打印第一个浮点数
+#print(get_real(s,4))           #打印第二个浮点数
+
+
 
 #写入浮点数
-plc.write_area(0x84,1,8,s)    #写入浮点数
+
+#将float数转为四字节数组
+def floatToBytes(f):
+    bs = struct.pack("f",f)
+    return (bs[3],bs[2],bs[1],bs[0])
+
+b=floatToBytes(999.8)          
+print(b[1])
+plc.write_area(0x84,1,8,bytearray([b[0], b[1], b[2], b[3]]))    #写入浮点数
 
 
 
 
+'''
 #读取布尔量
 
 t=plc.read_area(0x84,2,0,2)   #读取DB2从第0个字节开始，连续的2个字节
 print(get_bool(t,0,1))      #打印第0个字节（从0开始）的第1位
 #print(get_bool(t,1,0))       #打印第1个字节（从0开始）的第0位
-
+'''
 
 
 
